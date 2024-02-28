ff006081124c7b67bd0e328160d19445092caa41
daffahelmi <39628395+daffahelmi@users.noreply.github.com>
2024-02-29 02:49:49 +0700

    Update mar.sh

--- mar.sh
+++ mar.sh
@@ -63,7 +63,7 @@ wget -O /opt/marzban/.env "https://raw.githubusercontent.com/daffahelmi/MarLing/
 echo -e 'profile' >> /root/.profile
 wget -O /usr/bin/profile "https://raw.githubusercontent.com/daffahelmi/MarLing/main/profile";
 chmod +x /usr/bin/profile
-apt install screenfetch -y
+apt install neofetch -y
 wget -O /usr/bin/cekservice "https://raw.githubusercontent.com/daffahelmi/MarLing/main/cekservice.sh"
 chmod +x /usr/bin/cekservice
 
